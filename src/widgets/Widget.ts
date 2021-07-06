669e8a9097ba924122df62a09b2a60f84a337cfd
rafern <15365765+rafern@users.noreply.github.com>
2021-07-06 13:24:12 +0100

    Updated Widget API

--- src/widgets/Widget.ts
+++ src/widgets/Widget.ts
@@ -6,18 +6,15 @@ import type { Event } from '../events/Event';
 import type { Theme } from '../theme/Theme';
 import type { Root } from '../core/Root';
 
-// FIXME protected and private members were turned public due to a declaration
-// emission bug:
-// https://github.com/Microsoft/TypeScript/issues/17744
-// FIXME I would make this class abstract, but that would prevent Mixins from
-// working (see issue TypeScript#29653)
+// XXX This class is abstract, but marking it abstract breaks mixins. Instead,
+//     abstract methods are marked astract and TS errors are ignored
 export class Widget {
     // Is this widget enabled? If it isn't, it will act as if it didn't exist
-    #enabled = true;
+    private _enabled = true;
     // Widget will only be drawed if dirty is true
-    dirty = true;
+    protected dirty = true;
     // Widget will only have the layout resolved if layoutDirty is true
-    layoutDirty = true;
+    protected layoutDirty = true;
     // Widget will have its background cleared on draw if needsClear is true
     readonly needsClear: boolean;
     // Widget will get targetted events even if the target is not itself if it
@@ -27,110 +24,113 @@ export class Widget {
     // theme will be the inherited theme, else, it will be the theme override
     // with the inherited theme as the fallback. The fallback of the theme
     // override will be ignored and replaced
-    #themeOverride: Theme | null;
+    private _themeOverride: Theme | null;
     // The current theme in use by the Widget
-    #theme: Theme | null = null;
+    private _theme: Theme | null = null;
     // The inherited theme
-    #inheritedTheme: Theme | null = null;
+    private _inheritedTheme: Theme | null = null;
     // The resolved width and height
-    resolvedWidth = 0;
-    resolvedHeight = 0;
+    protected resolvedWidth = 0;
+    protected resolvedHeight = 0;
 
     // Constructor
     constructor(themeOverride: Theme | null, needsClear: boolean, propagatesEvents: boolean) {
         this.needsClear = needsClear;
         this.propagatesEvents = propagatesEvents;
-        this.#themeOverride = themeOverride;
+        this._themeOverride = themeOverride;
     }
 
     // Called when the inherited theme of this Widget is updated. Can be
     // overridden. Does nothing by default
-    updateInheritedTheme(): void {} // XXX protected
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    protected updateInheritedTheme(): void {}
 
     // Update this widget's current theme, with theme override set up. Must not
     // be overridden
-    updateTheme(): void { // XXX private
-        if(this.#themeOverride === null)
-            this.#theme = this.#inheritedTheme;
+    private updateTheme(): void {
+        if(this._themeOverride === null)
+            this._theme = this._inheritedTheme;
         else {
-            this.#themeOverride.fallback = this.#inheritedTheme;
-            this.#theme = this.#themeOverride;
+            this._themeOverride.fallback = this._inheritedTheme;
+            this._theme = this._themeOverride;
         }
     }
 
     // The current theme in use by the Widget. If there is no theme, throws an
     // exception
     get theme(): Theme {
-        if(this.#theme === null)
+        if(this._theme === null)
             throw 'Widget theme is not ready';
 
-        return this.#theme;
+        return this._theme;
     }
 
     // Is this widget enabled?
     get enabled(): boolean {
-        return this.#enabled;
+        return this._enabled;
     }
 
-    // Enable this widget
-    enable(): void {
-        if(!this.#enabled) {
-            this.#enabled = true;
-            this.layoutDirty = true;
-            this.dirty = true;
-        }
-    }
+    // Enable or disable this widget
+    set enabled(enabled: boolean) {
+        if(enabled === this._enabled)
+            return;
 
-    // Disable this widget
-    disable(): void {
-        if(this.#enabled) {
-            this.#enabled = false;
-            this.layoutDirty = true;
-            this.dirty = false;
-        }
+        this._enabled = enabled;
+        this.dirty = enabled;
+        this.layoutDirty = true;
     }
 
     // Set the theme override of this widget. Should not be overridden, but can
     // be. If overridden, the original method should still be called.
-    setThemeOverride(theme: Theme | null): void {
+    protected setThemeOverride(theme: Theme | null): void {
         // Abort if theme hasn't changed
-        if(this.#themeOverride === theme)
+        if(this._themeOverride === theme)
             return;
 
-        this.#themeOverride = theme;
+        this._themeOverride = theme;
         this.updateTheme();
 
-        if(this.#enabled) {
+        if(this._enabled) {
             this.layoutDirty = true;
             this.dirty = true;
         }
     }
 
-    // Get the theme override of this widget. Must not be overridden
-    getThemeOverride(): Theme | null {
-        return this.#themeOverride;
+    // Set the theme override of this widget. Calls setThemeOverride
+    set themeOverride(theme: Theme | null) {
+        this.setThemeOverride(theme);
+    }
+
+    // Get the theme override of this widget
+    get themeOverride(): Theme | null {
+        return this._themeOverride;
     }
 
     // Set the inherited theme of this Widget. Should not be overridden, but can
     // be. If overridden, the original method should still be called.
-    inheritTheme(theme: Theme): void {
+    protected inheritTheme(theme: Theme | null): void {
         // Abort if theme hasn't changed
-        if(this.#inheritedTheme === theme)
+        if(this._inheritedTheme === theme)
             return;
 
-        this.#inheritedTheme = theme;
+        this._inheritedTheme = theme;
         this.updateInheritedTheme();
         this.updateTheme();
 
-        if(this.#enabled) {
+        if(this._enabled) {
             this.layoutDirty = true;
             this.dirty = true;
         }
     }
 
-    // Get the inherited theme of this widget. Must not be overridden
-    getInheritedTheme(): Theme | null {
-        return this.#inheritedTheme;
+    // Set the theme override of this widget. Calls inheritTheme
+    set inheritedTheme(theme: Theme | null) {
+        this.inheritTheme(theme);
+    }
+
+    // Get the theme override of this widget
+    get inheritedTheme(): Theme | null {
+        return this._inheritedTheme;
     }
 
     // Called when a focus type owned by this Widget has been dropped. Does
@@ -142,7 +142,7 @@ export class Widget {
     // capturer is returned, else, null. By default, this will do nothing and
     // capture the event if it is targetted at itself or is a PointerEvent.
     // Should be overridden
-    handleEvent(event: Event, _width: number, _height: number, _root: Root): Widget | null { // XXX protected
+    protected handleEvent(event: Event, _width: number, _height: number, _root: Root): Widget | null {
         if(event.target === this ||
            ((event instanceof PointerEvent) && (event.target === null)))
             return this;
@@ -157,7 +157,7 @@ export class Widget {
     // the handleEvent method is called and its result is returned. Must not be
     // overridden
     dispatchEvent(event: Event, width: number, height: number, root: Root): Widget | null {
-        if(!this.#enabled)
+        if(!this._enabled)
             return null;
 
         if(event.target === null) {
@@ -174,12 +174,12 @@ export class Widget {
 
     // Does nothing by default. Should be implemented
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    handlePreLayoutUpdate(_root: Root): void {}
+    protected handlePreLayoutUpdate(_root: Root): void {}
 
     // Called before the layout is resolved. Calls its handler if widget is
     // enabled. Must not be implemented
     preLayoutUpdate(root: Root): void {
-        if(this.#enabled)
+        if(this._enabled)
             this.handlePreLayoutUpdate(root);
     }
 
@@ -187,31 +187,26 @@ export class Widget {
     // first stage, which fills the layout context with what the Widget wants in
     // terms of layout, while handleResolveLayout is called on the second stage,
     // where the final width and height are resolved. Must be implemented
-    // XXX I would make these abstract, but Typescript has a bug that prevents
-    // Mixins from being constrained to abstract classes, so this would prevent
-    // Mixins like Clickable from existing. See issue #29653:
-    // https://github.com/microsoft/TypeScript/issues/29653
-    handlePopulateLayout(_layoutCtx: LayoutContext): void { // XXX protected
-        throw new Error('Widget.handlePopulateLayout not implemented');
-    }
-
-    handleResolveLayout(_layoutCtx: LayoutContext): void { // XXX protected
-        throw new Error('Widget.handleResolveLayout not implemented');
-    }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore: "Abstract methods can only appear within an abstract class"
+    abstract protected handlePopulateLayout(layoutCtx: LayoutContext): void;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore: "Abstract methods can only appear within an abstract class"
+    abstract protected handleResolveLayout(layoutCtx: LayoutContext): void;
 
     // Wrappers for handlePopulateLayout and handleResolveLayout. Only call
     // callbacks when the layout is dirty, except when populating. If the layout
     // was dirty and is resolved, the dirty flag is also set (used for
     // painting). Must not be overridden
     populateLayout(layoutCtx: LayoutContext): void {
-        if(!this.#enabled)
+        if(!this._enabled)
             return;
 
         this.handlePopulateLayout(layoutCtx);
     }
 
     resolveLayout(layoutCtx: LayoutContext): void {
-        if(!this.#enabled) {
+        if(!this._enabled) {
             this.resolvedWidth = 0;
             this.resolvedHeight = 0;
             this.layoutDirty = false;
@@ -234,7 +229,7 @@ export class Widget {
     // Forcefully mark layout as dirty. If overridden, original must be called.
     // Call only when absolutely neccessary, such as in a resize
     forceLayoutDirty(): void {
-        if(this.#enabled) {
+        if(this._enabled) {
             this.layoutDirty = true;
             this.dirty = true;
         }
@@ -242,17 +237,17 @@ export class Widget {
 
     // Does nothing by default. Should be implemented
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    handlePostLayoutUpdate(_root: Root): void {}
+    protected handlePostLayoutUpdate(_root: Root): void {}
 
     // Called after the layout is resolved. Calls its handler if widget is
-    // enabled. Must not be implemented
+    // enabled. Must not be overridden
     postLayoutUpdate(root: Root): void {
-        if(this.#enabled)
+        if(this._enabled)
             this.handlePostLayoutUpdate(root);
     }
 
     // Paiting utility: clears background of widget. Should not be overridden
-    clear(x: number, y: number, width: number, height: number, ctx: CanvasRenderingContext2D): void { // XXX protected
+    protected clear(x: number, y: number, width: number, height: number, ctx: CanvasRenderingContext2D): void {
         ctx.save();
         ctx.globalCompositeOperation = 'copy';
         ctx.fillStyle = this.theme.getFill(ThemeProperty.CanvasFill);
@@ -268,7 +263,7 @@ export class Widget {
     // Widget painting callback. By default does nothing. Do painting logic here
     // when extending Widget. Should be overridden
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    handlePainting(_x: number, _y: number, _width: number, _height: number, _ctx: CanvasRenderingContext2D): void {} // XXX protected
+    protected handlePainting(_x: number, _y: number, _width: number, _height: number, _ctx: CanvasRenderingContext2D): void {}
 
     // Called when the Widget is dirty and the Root is being rendered. Does
     // nothing if dirty flag is not set, else, clears the background if
@@ -280,7 +275,7 @@ export class Widget {
 
         //console.log('Painted', this.constructor.name);
 
-        if(this.#enabled) {
+        if(this._enabled) {
             if(this.needsClear)
                 this.clear(x, y, width, height, ctx);
 
