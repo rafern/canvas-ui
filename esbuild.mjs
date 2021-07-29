89fd0d96ffc910770445c58cd841714aa05de9bb
rafern <rafern@protonmail.com>
2021-07-29 11:16:17 +0100

    Use lws instead of local-web-server, add dev npm script

---
+++ esbuild.mjs
@@ -0,0 +1,41 @@
+import Lws from 'lws';
+
+let devMode = false;
+for(const arg of process.argv.slice(2)) {
+    if(arg == '--dev')
+        devMode = true;
+}
+
+if(devMode) {
+    console.log('Development mode enabled; files will be watched and a test server started');
+    console.warn('Note that .d.ts files are not generated in this mode');
+    Lws.create({ directory: 'examples', https: true }).then(lws => {
+        lws.on('verbose', (key, value) => {
+            if(key === 'server.listening') {
+                const urls = [];
+                for(const ip of value) {
+                    if('url' in ip)
+                        urls.push(ip.url);
+                }
+
+                console.log(`Server listening on ${urls.join(', ')}`);
+            }
+            else if(key === 'server.error')
+                console.log(`Server error: ${value}`);
+            else if(key === 'server.closed')
+                console.log('Server socket closed');
+        })
+    });
+}
+
+import('esbuild').then(esbuild => esbuild.build({
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    watch: devMode,
+    target: 'es6',
+    format: 'esm',
+    logLevel: 'info',
+    entryPoints: [ './src/index.ts' ],
+    outfile: './lib/index.esm.js',
+}).catch(() => process.exit(1)));
